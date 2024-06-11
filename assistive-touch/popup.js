090311c6f8329b72713801f1f40426ded7580705
Tanay Sahajwalla <142775586+Tanay1805@users.noreply.github.com>
2024-06-11 10:20:55 +0530

    Add files via upload

---
+++ assistive-touch/popup.js
@@ -0,0 +1,24 @@
+document.getElementById("youtube").addEventListener("click", () => {
+    window.open("https://www.youtube.com", "_blank");
+});
+
+document.getElementById("stackoverflow").addEventListener("click", () => {
+    window.open("https://stackoverflow.com", "_blank");
+});
+
+document.getElementById("leetcode").addEventListener("click", () => {
+    window.open("https://leetcode.com", "_blank");
+});
+
+document.getElementById("linkedin").addEventListener("click", () => {
+    window.open("https://www.linkedin.com", "_blank");
+});
+
+document.getElementById("github").addEventListener("click", () => {
+    window.open("https://github.com", "_blank");
+});
+
+document.getElementById("twitter").addEventListener("click", () => {
+    window.open("https://twitter.com", "_blank");
+});
+
